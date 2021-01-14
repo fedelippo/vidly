ac03b43f0a76cdea38d18173b8cca059b40c0d96
Filippo Biondi <filomail@gmail.com>
2021-01-14 19:36:32 +0000

    Add genre filtering

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -17,7 +17,8 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() });
+    const genres = [{ name: "All Genres" }, ...getGenres()];
+    this.setState({ movies: getMovies(), genres });
   }
 
   handleDelete = (movie) => {
@@ -41,15 +42,24 @@ class Movies extends Component {
 
   handleGenreSelected = (genre) => {
     console.log("Handle genre selected ", { genre });
-    this.setState({ selectedGenre: genre });
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
   render() {
     const { length: count } = this.state.movies;
-    const { currentPage, pageSize, movies: allMovies } = this.state;
+    const {
+      currentPage,
+      pageSize,
+      movies: allMovies,
+      selectedGenre,
+    } = this.state;
     if (count === 0) return <p>No movies in the database.</p>;
 
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((movie) => movie.genre._id === selectedGenre._id)
+        : allMovies;
+    const movies = paginate(filtered, currentPage, pageSize);
 
     return (
       <div className="row">
@@ -61,7 +71,7 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>Found {count} movies in the database</p>
+          <p>Found {filtered.length} movies in the database</p>
           <table className="table">
             <thead>
               <tr>
@@ -98,7 +108,7 @@ class Movies extends Component {
             </tbody>
           </table>
           <Pagination
-            itemsCount={count}
+            itemsCount={filtered.length}
             pageSize={pageSize}
             onPageChange={this.handlePageChange}
             currentPage={currentPage}
