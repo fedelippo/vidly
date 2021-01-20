249d2276d1884c58bd66e1c6fa7dbfaa8b403860
Filippo Biondi <filomail@gmail.com>
2021-01-20 22:55:08 +0000

    Add Login form and preventDefult method

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from "./components/common/notFound";
 import NavBar from "./components/common/navBar";
 import React, { Component } from "react";
 import MovieForm from "./components/common/movieForm";
+import LoginForm from "./components/loginForm";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
