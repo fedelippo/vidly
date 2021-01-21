930c7c390fd3541d220e8cb39c76cbd27f3313fb
Filippo Biondi <Filippo.Biondi@stepstone.com>
2021-01-21 19:10:36 +0000

    Add username form property

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 class LoginForm extends Component {
+
+  state = {
+    account: {username:'', password:''}
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("Submitted!");
   };
+
+  handleChange = e => {
+    const account = {...this.state.account};
+    account.username = e.currentTarget.value;
+    this.setState({account});
+  }
+  
   render() {
     return (
       <div>
@@ -11,7 +23,7 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input value={this.state.account.username} onChange={this.handleChange} id="username" type="text" className="form-control" />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
